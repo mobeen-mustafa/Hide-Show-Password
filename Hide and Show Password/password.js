b85cf864a633c278fbcd39da330c2c7932100ce0
mobeen-mustafa <158339947+mobeen-mustafa@users.noreply.github.com>
2024-02-10 14:36:58 +0500

    Add files via upload

---
+++ Hide and Show Password/password.js
@@ -0,0 +1,17 @@
+
+let eyeicon=document.querySelector("#eye-icon")
+let password=document.querySelector("#password")
+
+
+
+eyeicon.onclick = function(){
+
+    if(password.type==="password"){
+        password.type="text";
+        eyeicon.src="eye-open.png"
+    }
+    else{
+           password.type = "password"
+           eyeicon.src="eye-close.png"
+    }
+}
